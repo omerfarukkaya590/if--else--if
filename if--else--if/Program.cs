deb74ff3f6598c668ea556c2a131725a7757589d
omerfarukkaya590 <85821993+omerfarukkaya590@users.noreply.github.com>
2022-01-14 21:18:45 +0300

    Add files via upload

---
+++ if--else--if/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace if__else__if
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int time =DateTime.Now.Hour;
+
+            if(time>=6 && time<11)
+            Console.WriteLine("Günaydın");
+            if(time<=18)
+            Console.WriteLine("İyi Günler");
+            else
+            Console.WriteLine("İyi Geceler");
+            string sonuc= time<=18 ? "iyi günler" : "İyi Geceler";
+
+            sonuc = time>=6 && time<11 ? "Günaydın" : time<= 18 ? "İyi Günler" : "İyi Geceler";
+            Console.WriteLine(sonuc);
+        }
+    }
+}
